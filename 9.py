1dbb682fdcaaf10b15d26af705703403eb8b4e05
Gzlng <chilikin.kolya@gmail.com>
2026-03-11 17:31:33 +0300

    task_9

---
+++ 9.py
@@ -0,0 +1,14 @@
+h1 = 1
+m1 = 1
+s1 = 1
+
+h2 = 2
+m2 = 2
+s2 = 2
+
+seconds1 = h1 * 3600 + m1 * 60 + s1
+seconds2 = h2 * 3600 + m2 * 60 + s2
+
+difference = seconds2 - seconds1
+
+print(difference)
